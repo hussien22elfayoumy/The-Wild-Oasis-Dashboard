7aa9554094b14c94d74cff8f6b6e35ad48529f68
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-02-22 22:44:29 +0200

    fix actibe link in ui

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,12 +3,12 @@ import Sidebar from '../components/global/SideBar';
 
 export default function AppLayout() {
   return (
-    <div className="grid h-screen grid-cols-[26rem_1fr] grid-rows-[auto_1fr]">
-      <header className="bg-my-grey-0 border-my-grey-200 border-b p-3 px-12">
+    <div className="grid min-h-screen grid-cols-[26rem_1fr] grid-rows-[auto_1fr]">
+      <header className="bg-my-grey-0 border-my-grey-200 border-b px-12 py-5">
         Header
       </header>
       <Sidebar />
-      <main className="bg-my-grey-50 overflow-scroll p-10 pb-16">
+      <main className="bg-my-grey-50 overflow-auto p-10 pb-16">
         <div className="mx-auto flex max-w-[120rem] flex-col gap-8">
           <Outlet />
         </div>
